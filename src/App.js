ab7c79f01f75b04a9f7d78592262e1a448b311e1
SilentTush <tkushwaha70@gmail.com>
2022-05-22 14:11:14 +0530

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import './App.css';
-import { Container,Row, Col } from 'react-bootstrap';
-import Signin from "./UserAccouts/signin"
-import Signup from "./UserAccouts/signup"
-import Home from "./UserAccouts/Home"
+import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
+import Signin from "./UserAccouts/signin";
+import Signup from "./UserAccouts/signup";
+import Home from "./UserAccouts/Home";
 import ProtectedRoute from "./UserAccouts/ProtectedRoute";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Row>
-          <Col>
-          <Routes>
-          <Route
-                path="/home"
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-            <Route path="/" element={<Signin/>}></Route>
-            <Route path="/signup" element={<Signup/>}></Route>
-          </Routes>
-          </Col>
-        </Row>
-      </Container>
-      
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Signin />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../styles/home/home.module.scss";
+
+export default function HomePage() {
+  return <div className={styles.home}>Home</div>;
+}
